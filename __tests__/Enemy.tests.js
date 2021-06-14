d6af4ae3df9c1966d700d5f0b4ee35d575007ba1
ShareeO <selena74354@yahoo.com>
2021-06-13 23:19:28 -0500

    snapshot 10.4.7

--- __tests__/Enemy.tests.js
+++ __tests__/Enemy.tests.js
@@ -57,3 +57,4 @@ test("gets enemy's health value", () => {
     expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
     expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
   });
+  
